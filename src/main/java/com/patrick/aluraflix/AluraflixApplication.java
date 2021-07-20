a7232b88165dd8c5db3f01d73e84cfaac24e65c1
Patrick Pires <patrick.lourenco@globalhitss.com.br>
2021-07-20 08:59:12 -0300

    setting connection to database

--- src/main/java/com/patrick/aluraflix/AluraflixApplication.java
+++ src/main/java/com/patrick/aluraflix/AluraflixApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AluraflixApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AluraflixApplication.class, args);
 	}
-
 }
