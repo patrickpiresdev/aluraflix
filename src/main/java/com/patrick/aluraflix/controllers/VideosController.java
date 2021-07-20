9f3b081c613c4b98abecc793072e449a91ac3e3f
Patrick Pires <patrick.lourenco@globalhitss.com.br>
2021-07-20 13:09:19 -0300

    feat: creating method to detail a video by id

--- src/main/java/com/patrick/aluraflix/controllers/VideosController.java
+++ src/main/java/com/patrick/aluraflix/controllers/VideosController.java
@@ -1,13 +1,17 @@
 package com.patrick.aluraflix.controllers;
 
 import com.patrick.aluraflix.controllers.dto.VideoDto;
+import com.patrick.aluraflix.models.Video;
 import com.patrick.aluraflix.repositories.VideosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,5 +24,14 @@ public class VideosController {
     @GetMapping
     public List<VideoDto> list() {
         return videosRepository.findAll().stream().map(VideoDto::new).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VideoDto> detail(@PathVariable Long id) {
+        Optional<Video> video = videosRepository.findById(id);
+
+        return video.isPresent() ?
+                ResponseEntity.ok(new VideoDto(video.get())) :
+                ResponseEntity.notFound().build();
     }
 }
