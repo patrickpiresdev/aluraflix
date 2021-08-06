212d4a422233be566a0cc935b9aaa8c92be3bb5e
Patrick Pires <patrick.lourenco@globalhitss.com.br>
2021-08-06 06:24:23 -0300

    feat: url mapping to detail a category by id

--- src/main/java/com/patrick/aluraflix/controllers/CategoriasController.java
+++ src/main/java/com/patrick/aluraflix/controllers/CategoriasController.java
@@ -1,13 +1,17 @@
 package com.patrick.aluraflix.controllers;
 
 import com.patrick.aluraflix.controllers.dto.CategoriaDto;
+import com.patrick.aluraflix.models.Categoria;
 import com.patrick.aluraflix.repositories.CategoriasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,5 +24,12 @@ public class CategoriasController {
     @GetMapping
     public List<CategoriaDto> list() {
         return categoriasRepository.findAll().stream().map(CategoriaDto::new).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDto> detail(@PathVariable Long id) {
+        Optional<Categoria> categoria = categoriasRepository.findById(id);
+        if (categoria.isPresent()) return ResponseEntity.ok(new CategoriaDto(categoria.get()));
+        return ResponseEntity.notFound().build();
     }
 }
